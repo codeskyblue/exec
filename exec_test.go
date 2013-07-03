691a6cda5d9e01a7e81b7d07ec9de254c3b76c1e
shxsun <shxsun@db3001.vm.baidu.com>
2013-07-03 11:34:33 +0800

    add more test

--- exec_test.go
+++ exec_test.go
@@ -1,9 +1,38 @@
 package exec
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
-func TestOK(t *testing.T) {
-	if 1 != 1 {
-		t.Error("my god")
+func TestNormal(t *testing.T) {
+	cmd := Command("echo", "-n", "hello")
+	output, err := cmd.Output()
+	if err != nil {
+		t.Errorf("expect normal, but got error: %s\n", err.Error())
 	}
+	if string(output) != "hello" {
+		t.Errorf("expect 'hello', but got: %s\n", string(output))
+	}
+}
+
+// FIXME
+func TestNormalTimeout(t *testing.T) {
+}
+
+func TestTimeout(t *testing.T) {
+	cmd := Command("sleep", "2")
+	cmd.Timeout = time.Second * 1
+	_, err := cmd.Output()
+	if err != ErrTimeout {
+		t.Errorf("expect ErrTimeout, but got: %s\n", err.Error())
+	}
+}
+
+// FIXME
+func TestCleanNormal(t *testing.T) {
+}
+
+// FIXME
+func TestCleanTimeout(t *testing.T) {
 }
